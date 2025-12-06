3dbf5928883c190579973a1b1d8ec30ec9967f0c
tanaevva.27 <vovan.tanaev@gmail.com>
2025-12-06 14:16:10 +0300

    added human.java

---
+++ human.java
@@ -0,0 +1,8 @@
+public abstract class human { //идейно в игре есть n различных людей, которые будут взаимодействоать с персонажем на основе рассчитываемых параметров
+    String name;
+    int age;
+    double reputation;
+    int IQ;
+
+}
+
